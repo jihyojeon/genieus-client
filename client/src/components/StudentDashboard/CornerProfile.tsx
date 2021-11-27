72c7f32fd1c44ecfb912c7e198f0e840fb9ca297
Dave Martin <david.martin@tookay.net>
2021-11-27 13:02:37 +0000

    feat(page): no real progress, just commiting prior to lunch

--- client/src/components/StudentDashboard/CornerProfile.tsx
+++ client/src/components/StudentDashboard/CornerProfile.tsx
@@ -14,25 +14,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCrown } from '@fortawesome/free-solid-svg-icons'
 
 // TODO: pull from database/state/props
-const name: string = "Louis"
-const tier: string = "Pro"
-const tierIcon: any = "test"
+const name: string = 'Louis'
+const tier: string = 'Pro'
+// TODO: use font-awesome icons depending on tier
+const tierIcon: any = '[CrownIcon]'
 
 const CornerProfile = () => {
-  return (
-    <Flex flexDirection={"row"}>
-      <Flex flexDirection={"column"}>
-        <Heading>Welcome {name}</Heading>
-        <Text>{tier} Tier {tierIcon}</Text>
-      </Flex>
-      <Image
-        src="https://bit.ly/sage-adebayo"
-        boxSize="60px"
-        borderRadius="50px"
-        m="5"
-      />
-    </Flex>
-  )
+    return (
+        <Flex flexDirection={'row'}>
+            <Flex flexDirection={'column'}>
+                <Heading>Welcome {name}</Heading>
+                <Text>
+                    {tier} Tier {tierIcon}
+                </Text>
+            </Flex>
+            <Image
+                src="https://bit.ly/sage-adebayo"
+                boxSize="10rem"
+                borderRadius="5rem"
+                ml="5"
+            />
+        </Flex>
+    )
 }
 
 export default CornerProfile
--- client/src/components/StudentDashboard/CreateRequestButton.tsx
+++ client/src/components/StudentDashboard/CreateRequestButton.tsx
@@ -9,24 +9,23 @@ const clickHandler = () => {
 }
 
 const CreateRequestButton = () => {
-    return (
-        <Box>
-            <Flex justify={'flex-start'} align={'center'}>
-                <Button
-                    // TODO: format here or in ButtonStyle.ts?
-                    bg={buttonColorPurple}
-                    borderRadius={'2.5rem'}
-                    boxShadow="dark-lg"
-                    fontSize="3xl"
-                    fontWeight={600}
-                    height={'5rem'}
-                    onClick={clickHandler}
-                    width={'20rem'}
-                >
-                    Create Request
-                </Button>
-            </Flex>
-        </Box>
+  return (
+      // TODO: flipping annoying button isn't aligning vertical center!
+        <Flex justify={'flex-start'} align={'center'}>
+            <Button
+                // TODO: format here or in ButtonStyle.ts?
+                bg={buttonColorPurple}
+                borderRadius={'2.5rem'}
+                boxShadow="dark-lg"
+                fontSize="3xl"
+                fontWeight={600}
+                height={'5rem'}
+                onClick={clickHandler}
+                width={'20rem'}
+            >
+                Create Request
+            </Button>
+        </Flex>
     )
 }
 
