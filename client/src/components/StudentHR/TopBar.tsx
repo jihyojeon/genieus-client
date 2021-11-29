4af881606d40be2361eccf145ecff1b4fbb11307
Louis <louis.cappell@icloud.com>
2021-11-29 10:15:59 +0000

    feat(app/studenthr): added new route to Student-hr

---
+++ client/src/components/StudentHR/TopBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Heading, Box, Flex} from '@chakra-ui/react'
+
+const TopBar = () => {
+  return (
+    <Flex>
+
+      
+      
+    </Flex>
+  )
+}
+
+export default TopBar
