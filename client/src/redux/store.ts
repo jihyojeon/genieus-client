055b2e6087aba4371713da9bdbefad733f04bedf
Louis <louis.cappell@icloud.com>
2021-11-30 17:28:32 +0000

    clean up

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-// import tutorReducer from './features/tutorSlice'
+
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { tutorApi } from './services/tutorService'
 
 export const store = configureStore({
   reducer: {
-    // tutor: tutorReducer,
     [tutorApi.reducerPath]: tutorApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
@@ -13,7 +12,3 @@ export const store = configureStore({
 })
 
 setupListeners(store.dispatch)
-// Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<typeof store.getState>
-// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-// export type AppDispatch = typeof store.dispatch
