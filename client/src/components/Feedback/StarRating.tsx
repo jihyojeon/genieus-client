da2f254aded71bdc36a59043a8a5f1ea377edfc2
Dave Martin <david.martin@tookay.net>
2021-12-02 11:08:17 +0000

    refactor(feedback): refining code. Commit before revising star rating

--- client/src/components/Feedback/StarRating.tsx
+++ client/src/components/Feedback/StarRating.tsx
@@ -1,39 +1,28 @@
 import React, { useState } from 'react'
 import {
-  Avatar,
   Flex,
-  Heading,
+  HStack,
   Text,
-  useColorModeValue,
-  VStack,
+  // useColorModeValue
 } from '@chakra-ui/react'
 import { Rating } from 'react-simple-star-rating'
 
-const tutorObj = {
-  tutorName: 'Vic',
-  callDuration: 25,
-  photo: 'https://bit.ly/sage-adebayo',
-}
-
 const StarRating = () => {
-  const [tutorObjState, setTutorObjState] = useState(tutorObj)
+  // TODO: IMPLEMENT RETURNING OF STAR VALUE TO STUDENTCOMMENTS.TSX AND TUTOR'S RECORD ON SUBMIT
   const [rating, setRating] = useState(0)
-
   const handleRating = (rate: number) => {
     setRating(rate)
     console.log('Stars: ', rate)
-    // TODO: IMPLEMENT RETURNING OF STAR VALUE TO STUDENTCOMMENTS.TSX AND TUTOR'S RECORD ON SUBMIT
   }
 
+  // TODO: FIX THESE STARS TO DISPLAY HORIZONTALLY
   return (
-    <Flex direction="column" align="center">
-      <VStack spacing={4}>
-        <Text>You were online for {tutorObjState.callDuration} minutes</Text>
-        <Heading>How was your call with {tutorObjState.tutorName}?</Heading>
-        <Avatar size="xl" src={tutorObjState.photo} />
-        {/* TODO: FIX THESE STARS TO DISPLAY HORIZONTALLY */}
-        <Rating onClick={handleRating} ratingValue={rating} />
-      </VStack>
+    <Flex direction="row">
+      <HStack>
+      <Text>TEST</Text>
+
+      <Rating onClick={handleRating} ratingValue={rating} />
+      </HStack>
     </Flex>
   )
 }
