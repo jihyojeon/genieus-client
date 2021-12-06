f5932549247470b956af9099c210030e3329fde2
Alistair <alistairgarioch@gmail.com>
2021-12-06 09:23:18 +0000

    refactor(feedback): update feedback form styling

--- client/src/components/Feedback/StarRating.tsx
+++ client/src/components/Feedback/StarRating.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import { Box, Icon, Heading, Image, VStack } from '@chakra-ui/react'
-import { SunIcon } from '@chakra-ui/icons'
-import StarIcon from '../../assets/icons/star.svg'
+import { IconButton, HStack } from '@chakra-ui/react'
+import { FaStar } from 'react-icons/fa'
 
 const StarRating = ({ size, icon, scale, fillColor, strokeColor }: any) => {
   const [rating, setRating] = useState(0)
-  // TODO: 1) RETURN "rating" TO PARENT COMPONENT FOR SUBMISSION ON SUBMIT CLICK 
+
   const buttons = []
 
-  const onClick = (idx: any) => {
+  const clickHandler = (idx: any) => {
     if (!isNaN(idx)) {
       if (rating === 1 && idx === 1) {
         setRating(0)
@@ -18,63 +17,27 @@ const StarRating = ({ size, icon, scale, fillColor, strokeColor }: any) => {
     }
   }
 
-  const RatingIcon = ({ fill }: any) => {
-    return (
-      
-      // TODO: 2) REPLACE SUN ICONS WITH STAR ICONS BELOW
-          <SunIcon
-            size={`${size}px`}
-            color={fill ? "red" : "blue" }
-            stroke={strokeColor}
-            onClick={onClick}
-            fillOpacity={fill ? '100%' : '0'}
-          />
-
-        // <Image
-        //   src={StarIcon}
-        //   size={`${size}px`}
-        //   color={fill ? 'red' : 'blue'}
-        //   stroke={strokeColor}
-        //   onClick={onClick}
-        //   fillOpacity={fill ? '100%' : '0'}
-        // />
-    )
-  }
-
   const RatingButton = ({ idx, fill }: any) => {
     return (
-      <Box
-        // variant="unstyled"
-        as="button"
+      <IconButton
+        icon={<FaStar size="1.6rem" />}
         aria-label={`Rate ${idx}`}
-        height={`${size}px`}
-        width={`${size}px`}
-        mx={1}
-        onClick={() => onClick(idx)}
-        _focus={{ outline: 0 }}
-      >
-        <RatingIcon fill={fill} />
-      </Box>
+        borderRadius="50%"
+        variant="ghost"
+        size="md"
+        color={fill ? 'indigo.600' : 'gray.300'}
+        onClick={() => clickHandler(idx)}
+        fillOpacity={fill ? '100%' : '0'}
+      />
     )
   }
-
   for (let i = 1; i <= scale; i++) {
     buttons.push(<RatingButton key={i} idx={i} fill={i <= rating} />)
   }
-
   return (
-    <Box>
-
-    <VStack justify="center" mt={4}>
-      <Heading size="sm">
-      2. Please rate your tutor
-    </Heading>
-      </VStack>
-    <VStack isInline mt={2} justify="center">
-      {/* TODO: ADD LINE SPACING */}
+    <HStack isInline justify="center">
       {buttons}
-    </VStack>
-    </Box>
+    </HStack>
   )
 }
 
--- client/src/pages/Feedback.tsx
+++ client/src/pages/Feedback.tsx
@@ -1,72 +1,143 @@
-import { useState } from 'react'
+import React from 'react'
 import {
   Avatar,
   Button,
-  Center,
+  Box,
   Checkbox,
   Flex,
   Heading,
   Text,
   VStack,
+  Icon,
+  Textarea,
+  FormControl,
+  FormHelperText,
+  FormLabel,
 } from '@chakra-ui/react'
 import StarRating from '../components/Feedback/StarRating'
-import StudentComments from '../components/Feedback/StudentComments'
-import { useNavigate } from 'react-router-dom'
+import { FaHeart, FaBan } from 'react-icons/fa'
+import { useNavigate, useLocation } from 'react-router-dom'
 
-// TODO: REPLACE WITH DATABASE DATA FOR TUTOROBJSTATE
-const tutorObj = {
-  tutorName: 'Vic',
-  callDuration: 25,
-  photo: 'https://bit.ly/sage-adebayo',
+const mockHelpRequest = {
+  id: '73668645-b761-4ae2-ac96-e1a52dde2ac8',
+  status: 'closed-compelted',
+  description: 'this is a description',
+  time_opened: '2021-12-02T15:53:37.806Z',
+  time_accepted: '2021-12-02T15:53:37.806Z',
+  time_closed: '2021-12-02T15:53:37.806Z',
+  rating: null,
+  feedback_comments: null,
+  tags: null,
+  language: 'fortran',
+  code: 'this is some code',
+  zoom_url: 'http://zoom.com/132123412341234',
+  call_length: 211,
+  favourites_only: false,
+  tutor_id: 'sIOHhUgNX8PNU0eDXHAKM2Lnpz43',
+  student_id: 'spammyboi23',
+  interested_tutors: ['sIOHhUgNX8PNU0eDXHAKM2Lnpz43'],
+  declined_tutors: null,
+  createdAt: '2021-12-02T15:53:37.806Z',
+  updatedAt: '2021-12-02T15:53:37.806Z',
+  student: {
+    id: 'spammyboi23',
+    name: 'string',
+    photo_url: 'string',
+  },
+  tutor: {
+    id: 'sIOHhUgNX8PNU0eDXHAKM2Lnpz43',
+    name: 'Obama',
+    photo_url: null,
+  },
 }
 
-
 const Feedback = () => {
-  const [tutorObjState, setTutorObjState] = useState(tutorObj)
   const navigate = useNavigate()
-  
-  // TODO: IMPLEMENT ACTION ON SUBMIT -> UPDATE DATABASE -> RETURN TO DASHBOARD
+  // TODO: get help request from location state or other approach
+  // const location = useLocation()
+  // const helpRequest = location.state
+
   const submitHandler = () => {
-    console.log('Submit clicked')
     navigate('/student-dashboard')
   }
-
+  const helpRequest = mockHelpRequest
+  const { tutor } = mockHelpRequest
   return (
-    <Center>
+    <Box maxW="lg" margin="auto">
       <Flex
+        border={'1px solid'}
+        borderColor="gray.100"
         direction="column"
+        shadow="lg"
         align="center"
         mt="2rem"
-        border="solid"
-        borderRadius="3rem"
+        borderRadius="2rem"
         padding="2rem"
       >
-        <VStack spacing={4}>
-          <Text>You were online for {tutorObjState.callDuration} minutes</Text>
-          <Heading>How was your call with {tutorObjState.tutorName}?</Heading>
-          <Avatar size="2xl" src={tutorObjState.photo} />
-          <Heading size="sm">
-            1. Add {tutorObjState.tutorName} to your favourites?
-          </Heading>
-          <Checkbox defaultIsNotChecked mt="1rem">
-            {/* TODO: PASS TICKBOX OUTCOME TO PARENT COMPONENT FOR RECORING ON SUBMIT */}
-          </Checkbox>
-          <StarRating
-            size={32}
-            icon="StarIcon"
-            scale={5}
-            fillColor="gold"
-            strokeColor="grey"
-          />
-          <StudentComments name={tutorObjState.tutorName} />
-          <Flex direction="row">
-            <Button mt="1rem" onClick={submitHandler}>
-              Submit
-            </Button>
-          </Flex>
-        </VStack>
+        <form>
+          <VStack spacing="1.5rem">
+            <Heading textAlign="center">
+              How was your call with {tutor.name}?
+            </Heading>
+            <Text color="gray.500" textAlign="center">
+              You were on a call for {Math.floor(helpRequest.call_length / 60)}{' '}
+              minutes, {helpRequest.call_length % 60} seconds
+            </Text>
+            {tutor.photo_url && <Avatar size="2xl" src={tutor.photo_url} />}
+            <StarRating
+              size={32}
+              icon="StarIcon"
+              scale={5}
+              fillColor="gold"
+              strokeColor="grey"
+            />
+            <FormControl>
+              <Textarea placeHolder="Add any comments here..." />
+            </FormControl>
+            <Flex alignItems="flex-start">
+              <FormControl>
+                <Checkbox
+                  defaultIsNotChecked
+                  colorScheme="green"
+                  _hover={{
+                    color: 'green.500',
+                    transition: 'color .2s linear',
+                  }}
+                >
+                  <Icon as={FaHeart} w={3} h={3} />
+                  &nbsp; Favourite
+                </Checkbox>
+                <FormHelperText>
+                  If you enjoyed working with {tutor.name} add them to your
+                  favourites. You can prioritize your favourite tutors in future
+                  requests.
+                </FormHelperText>
+              </FormControl>
+              <FormControl ml={6}>
+                <Checkbox
+                  defaultIsNotChecked
+                  colorScheme="red"
+                  _hover={{ color: 'red.500', transition: 'color .2s linear' }}
+                >
+                  <Icon as={FaBan} w={3} h={3} />
+                  &nbsp;Block
+                </Checkbox>
+                <FormHelperText>
+                  Everyone responds differently to certain teaching styles. If
+                  you prefer {tutor.name} isn't assigned to your future
+                  requests, check the box above.
+                </FormHelperText>
+              </FormControl>
+            </Flex>
+            <FormControl textAlign="center">
+              <Button mt="1rem" mb="2rem" onClick={submitHandler} type="button">
+                Submit
+              </Button>
+            </FormControl>
+          </VStack>
+        </form>
       </Flex>
-    </Center>
+    </Box>
   )
 }
 
