fe8968ba3d2e2c87c294a7877f997c16cf47470f
Dave Martin <david.martin@tookay.net>
2021-12-06 15:36:13 +0000

    feat(chat): updates to new Topbar

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -5,35 +5,34 @@ import {
 } from '@chakra-ui/react'
 import ChatLeftPane from '../components/Chat/LeftPane/ChatLeftPane'
 import ChatRightPane from '../components/Chat/RightPane/ChatRightPane'
-import ChatTopBar from '../components/Chat/TopPane/ChatTopBar'
+// import ChatTopBarOld from '../components/Chat/TopPane/ChatTopBar'
+import ChatTopBar from '../components/TopBar/TopBar'
 
 // TODO: FIXES REQUIRED:
 // TODO: 1. REFRESHING PAGE RESETS COUNTDOWN TIMER
 // TODO: DISPLAY WARNING IF ATTEMPTING TO CLOSE THIS WINDOW - ATTEMPT AT THIS IS COMMENTED OUT ABOVE AND BELOW?
 // TODO: 2. REFACTOR PROP-DRILLING (START IN CHAT PAGE?)
-  // import { useBeforeunload } from 'react-beforeunload'
+// import { useBeforeunload } from 'react-beforeunload'
 
-  
-  // useBeforeunload(
-  //   () =>
-  //     'Closing this tab will not allow you to mark the session as complete and will cause excessive billing.  Are you sure you wish to close this tab? '
-  // )
+// useBeforeunload(
+//   () =>
+//     'Closing this tab will not allow you to mark the session as complete and will cause excessive billing.  Are you sure you wish to close this tab? '
+// )
 
-  // CODE TO WARN AGAINST CLOSING THE CHAT WINDOW
-  // window.addEventListener("beforeunload", (ev) =>
-  // {
-  //     ev.preventDefault();
-  //     return ev.returnValue = 'Are you sure you want to close?';
-  // });
-  // componentDidMount: function() {
-  //   window.addEventListener('onbeforeunload', this.handleWindowClose);
-  // },
+// CODE TO WARN AGAINST CLOSING THE CHAT WINDOW
+// window.addEventListener("beforeunload", (ev) =>
+// {
+//     ev.preventDefault();
+//     return ev.returnValue = 'Are you sure you want to close?';
+// });
+// componentDidMount: function() {
+//   window.addEventListener('onbeforeunload', this.handleWindowClose);
+// },
 
-  // componentWillUnmount: function() {
-  //   window.removeEventListener('onbeforeunload', this.handleWindowClose);
-  // }
-  // // ----
-
+// componentWillUnmount: function() {
+//   window.removeEventListener('onbeforeunload', this.handleWindowClose);
+// }
+// // ----
 
 const Chat = () => {
   return (
@@ -45,6 +44,7 @@ const Chat = () => {
       templateRows="1fr auto"
     >
       <GridItem rowSpan={1} colSpan={3}>
+        {/* <ChatTopBarOld /> */}
         <ChatTopBar />
       </GridItem>
 
