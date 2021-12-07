e34b01969ac14f17e6e16a06ed1db232a2060d34
Dave Martin <david.martin@tookay.net>
2021-12-07 18:21:02 +0000

    feat(tutordashboard): potentially breaking styling changes to Tutor DB

--- client/src/components/TutorDashboard/PreviousRequestCard.tsx
+++ client/src/components/TutorDashboard/PreviousRequestCard.tsx
@@ -13,26 +13,29 @@ import {
   WrapItem,
   TagLabel,
   Tag,
-  Avatar
-} from '@chakra-ui/react';
-import { ProgrammingLanguages } from '../../assets/devicon/ProgrammingLanguages';
-import { displayTimeinHHMMSS, displayDate } from './utils';
-import { BiTimeFive } from "react-icons/bi";
-import { AiOutlineDollarCircle, AiOutlineCalendar, AiOutlineStar } from 'react-icons/ai'
+  Avatar,
+} from '@chakra-ui/react'
+import { ProgrammingLanguages } from '../../assets/devicon/ProgrammingLanguages'
+import { displayTimeinHHMMSS, displayDate } from './utils'
+import { BiTimeFive } from 'react-icons/bi'
+import {
+  AiOutlineDollarCircle,
+  AiOutlineCalendar,
+  AiOutlineStar,
+} from 'react-icons/ai'
 import { useNavigate } from 'react-router-dom'
-
 
 export default function PreviousRequestCard({ hr }: any) {
   const navigate = useNavigate()
   return (
-    <Center 
+    <Center
       py={2}
       fontFamily="montserrat"
       mx={6}
       my={2}
       bg={useColorModeValue('white', 'gray.800')}
-      minW={'17%'}
-      maxW={'17%'}
+      minW={'20ch'}
+      maxW={'20ch'}
     >
       <Box
         w={'full'}
@@ -43,30 +46,31 @@ export default function PreviousRequestCard({ hr }: any) {
         border="1px solid"
         borderColor="rgba(0, 66, 156, .4)"
         borderRadius="10px"
+        
       >
         <Stack
           textAlign={'center'}
           p={6}
           color={useColorModeValue('gray.800', 'white')}
           align={'center'}
-          >
+        >
           <Stack direction={'row'} align={'center'} justify={'center'}>
             <Text fontSize={'lg'} fontWeight={800}>
               {hr.student.name}
             </Text>
             <Box
-            my={'auto'}
-            color={useColorModeValue('gray.800', 'gray.200')}
-            alignContent={'center'}
+              my={'auto'}
+              color={useColorModeValue('gray.800', 'gray.200')}
+              alignContent={'center'}
             >
               <Avatar
-              size={'xs'}
-              src={hr.student.photo_url}
-              name={hr.student.name}
-              alt={'Author'}
-              css={{
-                border: '2px solid white',
-              }}
+                size={'xs'}
+                src={hr.student.photo_url}
+                name={hr.student.name}
+                alt={'Author'}
+                css={{
+                  border: '2px solid white',
+                }}
               />
             </Box>
           </Stack>
@@ -86,15 +90,15 @@ export default function PreviousRequestCard({ hr }: any) {
           </WrapItem>
         </Stack>
 
-        <Box bg={useColorModeValue('gray.50', 'gray.900')} px={6} py={10}>
+        <Box bg={useColorModeValue('gray.50', 'gray.900')} px={6} py={10} >
           <List spacing={3}>
             <ListItem>
               <ListIcon as={AiOutlineStar} />
               {hr.rating}
             </ListItem>
             <ListItem>
-              <ListIcon as={AiOutlineDollarCircle} />
-              ${(hr.call_length*40/60/60).toFixed(2)}
+              <ListIcon as={AiOutlineDollarCircle} />$
+              {((hr.call_length * 40) / 60 / 60).toFixed(2)}
             </ListItem>
             <ListItem>
               <ListIcon as={BiTimeFive} />
@@ -111,17 +115,18 @@ export default function PreviousRequestCard({ hr }: any) {
             w={'full'}
             rounded={'xl'}
             _hover={{
+              transform: 'translateY(-2px)',
+              boxShadow: 'lg',
             }}
-            _focus={{
-            }}
+            _focus={{}}
             onClick={() => {
               navigate('/previous-hr', { state: hr })
-            }}>
+            }}
+          >
             See Feedback
           </Button>
         </Box>
       </Box>
     </Center>
-  );
+  )
 }
-
